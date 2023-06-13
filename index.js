a57b009f5d11955aa0065a16df083d43889034f6
murzynatorapp <109036734+murzynatorapp@users.noreply.github.com>
2023-06-13 02:50:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ chat.on("message", function message(data) {
   let jsonDataSub = JSON.parse(jsonData.data);
   let jsonSender = jsonDataSub.sender;
   
-  let message = new Date().toLocaleDateString(pl-PL) + 
+  let message = new Date().toLocaleDateString('pl-PL') + 
   " "  + 
-  new Date().toLocaleTimeString(pl-PL) + 
+  new Date().toLocaleTimeString('pl-PL') + 
   " " + 
   jsonSender?.username + 
   ": " + jsonDataSub.content
